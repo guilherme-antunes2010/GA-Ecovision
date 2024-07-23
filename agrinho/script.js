f000b910b6bef066ae13df11fb3392114acd660b
Guilherme-Antunes <guilherme256578@gmail.com>
2024-07-23 01:11:07 -0300

    Add files via upload

---
+++ agrinho/script.js
@@ -0,0 +1,15 @@
+function img1(){
+    document.getElementById("figura").src ="../images/pics(1).png";
+}
+
+function img2(){
+    document.getElementById("figura").src ="../images/pics(2).png";
+}
+
+function img3(){
+    document.getElementById("figura").src ="../images/pics(3).png";
+}
+
+function img4(){
+    document.getElementById("figura").src ="../images/pics(4).png";
+}
